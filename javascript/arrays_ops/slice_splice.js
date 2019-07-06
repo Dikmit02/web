e927e70282b39d71d21ec9277da4b188f9e5587c
diksha mittal <dikshamittal21297@gmail.com>
2019-07-06 23:46:35 +0530

    splice added

--- javascript/arrays_ops/slice_splice.js
+++ javascript/arrays_ops/slice_splice.js
@@ -1,6 +1,7 @@
 let notes=['do','re','mi','fa','so','la','ti','do']
 //slice
-notes.slice(4,6)
+//notes.slice(4)
+//notes.slice(4,6)
 console.log(notes)
 
 //splice
@@ -13,10 +14,27 @@ console.log(notes)
 // console.log(omit)
 // console.log(notes)
 
-let omit=notes.splice(4,2)
-console.log(omit)
-console.log(notes)
+// let omit=notes.splice(4,2)// 2 is length
+// console.log(omit)
+// console.log(notes)
 
 // splice2
-notes.splice(4,2,'diksha')
+// notes.splice(0,2,'diksha') // first is replaced by diksha and second isremoved
+// console.log(notes)
+
+// notes.splice(0,2,'diksha','huhuhuhuhhh','huhuhduhejhdehwij2qwo') // first and second  is replaced by diksha ,huhuhuhuhhh and third is added and all the other arec shifted
+// console.log(notes)
+
+notes.splice(0,2,'diksha','diksha') // first and second  is replaced by diksha ,huhuhuhuhhh and third is added and all the other arec shifted
+console.log(notes)
+
+
+// concat
+console.log(notes.concat('sa','re','ga'))// a new array is created everytime the concat is called
+console.log(notes)
+
+
+
+console.log(notes.concat(['sa','re','ga']))// a new array is created everytime the concat is called
+
 console.log(notes)
