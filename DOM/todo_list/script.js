b34f9dc98920bba5b2a3d9ed3d3fa21656f71610
diksha mittal <dikshamittal21297@gmail.com>
2019-07-10 00:54:27 +0530

    complete todo

--- DOM/todo_list/script.js
+++ DOM/todo_list/script.js
@@ -4,17 +4,53 @@ window.onload=function(){
     let todolist=this.document.getElementById('todolist')
     
     addtask.onclick=function(){
-
-
         
         let li=document.createElement('li')
-        li.innerText=newtask.value
+        
+        //Add delete button
         let xbtn=document.createElement('button')
         xbtn.innerText='(X)'
         xbtn.onclick=function(){
             event.target.parentElement.remove()
         }
+
+        //Add up button
+        let upBtn=document.createElement('button')
+        upBtn.innerText="↑"
+        upBtn.onclick=function(){
+            //event.target=the up button
+            //event.target.parentElement=<li></li>
+            //event.target.parentElement.parentElement = todolist
+            event.target.parentElement.parentElement.insertBefore(
+                event.target.parentElement,// insert this elemnt
+                event.target.parentElement.previousElementSibling// before its sibling
+
+
+
+            )
+        }
+        
+
+        //Add downButton\
+        let downbtn=document.createElement('button')
+        downbtn.innerText="↓"
+        downbtn.onclick=function(){
+            event.target.parentElement.parentElement.insertBefore(
+                event.target.parentElement.nextElementSibling,//insert next elemnt
+                event.target.parentElement// before curent element
+            )
+        }
+
+
+
+        let taskspan=document.createElement('span')
+        taskspan.innerText=newtask.value
+        
         li.appendChild(xbtn)
+        li.appendChild(upBtn)
+        li.appendChild(downbtn)
+        li.appendChild(taskspan)
+        
         todolist.appendChild(li)
 
         
